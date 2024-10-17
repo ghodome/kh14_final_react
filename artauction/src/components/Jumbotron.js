49d88bbee881e9ad9c7b8705207f7be293ee7a0e
ghodome <vjaewook@gmail.com>
2024-10-17 16:01:39 +0900

    점보트론 생성

--- artauction/src/components/Jumbotron.js
+++ artauction/src/components/Jumbotron.js
@@ -5,6 +5,7 @@ const Jumbotron =(props)=>{
                 <div className="col-sm-10 offset-sm-1">
                     <div className="bg-dark p-3 rounded">
                         <h2 className=" text-light">{props.title}</h2>
+                        
                         <p className=" text-light">{props.content}</p>
                     </div>
                 </div>
