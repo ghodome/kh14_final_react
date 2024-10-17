96daf2dae9b48ee7dfd882c4da3b92273794db92
ghodome <vjaewook@gmail.com>
2024-10-17 17:46:55 +0900

    작가 목록, 등록 구현중

--- artauction/src/components/Artist/ArtistRegist.js
+++ artauction/src/components/Artist/ArtistRegist.js
@@ -1,11 +1,61 @@
+import { useCallback, useEffect, useState } from "react";
 import Jumbotron from "../Jumbotron";
+import axios from "axios";
 
 const ArtistRegist = ()=>{
+    //state
+    const [input, setInput]=useState({});
+    const [artistList,setArtistList]=useState([]);
+
+    //callback
+    const changeInput = useCallback((e)=>{
+        setInput({...input,
+            [e.target.name]:e.target.value,
+        })
+    },[input]);
+
+    const loadArtistList =useCallback(async ()=>{
+        const resp=await axios.get("http://localhost:8080/artist/");
+        setArtistList(resp.data);
+    },[artistList]);
+
+    //effect
+    useEffect(()=>{
+        loadArtistList();
+    },[])
     return (<>
-        <Jumbotron title="작가 등록"></Jumbotron>
+        <Jumbotron title="작가 목록"></Jumbotron>
         <div className="row">
             <div className="col">
-                
+                <div className="modal-body">
+                    <div className="input-group">
+                        <label className="form-check-label">작가</label>
+                        <input className="form-control"
+                            name="artistName" value={input.artistName}
+                            onChange={e=>changeInput(e)}></input>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col">
+                <ul className="list-group list-group-flush">
+                    {artistList.map(artist=>(
+                        <div key={artist.artistNo}>
+                            <div className="row mt-2">
+                                <div className="col-4">
+                                    <li className="list-group-item">{artist.artistNo}</li>
+                                </div>
+                                <div className="col-4">
+                                    <li className="list-group-item">{artist.artistName}</li>
+                                </div>
+                                <div className="col-4">
+                                    <li className="list-group-item">{artist.artistBirth} ~ {artist.artistDeath}</li>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </ul>
             </div>
         </div>
     </>)
