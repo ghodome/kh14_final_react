a03d400c8b7baf02ec00545117eb2265b45d9a9d
Parkhewoo <pkhwoo91@gmail.com>
2024-10-18 16:29:56 +0900

    notice

--- artauction/src/components/Notice/Notice.js
+++ artauction/src/components/Notice/Notice.js
@@ -29,43 +29,7 @@ const Notice = () => {
     };
 
     //view
-    return (<>
-      <div className="container mt-4">
-        <div className="row align-items-center">
-          <div className="col-9 text-center w-100">
-            <h2 >게시판</h2>
-          </div>
-          <div className="col-9">
-          </div>
-          <div className="col-3">
-            <div className="d-flex justify-content-end">
-              <input type="text" className="form-control me-2" placeholder="검색"/>
-              <button className="btn btn-primary" style={{ flexShrink: 0 }}>
-                검색
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-  <div className="container mt-4">
-    <table className="table table-bordered">
-      <thead className="table-white">
-      </thead>
-      <tbody className="table">
-        {noticeList.map(notice=>(
-            <tr key={notice.noticeNo}>
-                <td className="text-center">{notice.noticeType}</td>
-                <td>
-                    <NavLink to={"/notice/detail/"+notice.noticeNo}>{notice.noticeTitle}</NavLink>
-                    </td>
-                <td className="text-center">{notice.noticeWtime}</td>
-            </tr>
-            ))}
-      </tbody>
-    </table>
-  </div>
-
-
+    
     return (
         <>
             <div className="container mt-4">
