98391b77dbee0d0c4668adfb99afaaca84dbcbaf
ghodome <vjaewook@gmail.com>
2024-10-22 14:05:31 +0900

    앱js 라우터 삭제

--- artauction/src/App.js
+++ artauction/src/App.js
@@ -1,19 +1,14 @@
 
 import Container from './components/Container';
 import Menu from './components/Menu';
-// import { RecoilRoot, useRecoilState } from 'recoil';
 
 
 function App() {
 
 
   return (<>
-    {/* <RecoilRoot> */}
-      <HashRouter>
         <Menu />
         <Container />
-      </HashRouter>
-    {/* </RecoilRoot> */}
   </>
   );
 }
