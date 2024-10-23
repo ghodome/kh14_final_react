b1d3f7190e255ed2d2a7aa7eb2d9bb4e6a5c64dd
shwjdgus1234 <shwjdgus12345@naver.com>
2024-10-23 18:16:22 +0900

    websocket 비회원 채팅 금지

--- artauction/src/components/websocket/WebSocket.js
+++ artauction/src/components/websocket/WebSocket.js
@@ -4,98 +4,142 @@ import SockJS from "sockjs-client";
 import { Client } from "@stomp/stompjs";
 import moment from "moment";
 import "moment/locale/ko";
+import { useRecoilValue } from 'recoil';
+import axios from "axios";
+import { loginState, memberIdState, memberRankState } from '../../utils/recoil';
+
 moment.locale("ko");
-const WebSocket = ()=>{
-    //state
+
+const WebSocket = () => {
+    // state
     const [input, setInput] = useState("");
     const [messageList, setMessageList] = useState([]);
     const [client, setClient] = useState(null);
     const [connect, setConnect] = useState(false);
-    
-   
-    //effect
-    useEffect(()=>{
+
+    // recoil - 아이디 불러오기
+    const login = useRecoilValue(loginState);
+    const memberId = useRecoilValue(memberIdState);
+    const memberRank = useRecoilValue(memberRankState);
+
+    // token - 토큰 불러오기
+    const accessToken = axios.defaults.headers.common["Authorization"];
+    const refreshToken = window.localStorage.getItem("refreshToken") || window.sessionStorage.getItem("refreshToken");
+
+    // effect
+    useEffect(() => {
         connectToServer();
-        return ()=>{
-            disconnectFromServer();
-        }
-    }, []);
-    //callback
-    const connectToServer = useCallback(()=>{
+        return () => {
+            disconnectFromServer(client);
+        };
+    }, [login]);
+
+    // callback
+    const connectToServer = useCallback(() => {
         const socket = new SockJS("http://localhost:8080/ws");
         const client = new Client({
-            webSocketFactory : ()=>socket,
-            onConnect: ()=>{
-                client.subscribe("/public/chat", (message)=>{
+            webSocketFactory: () => socket,
+            onConnect: () => {
+                client.subscribe("/public/chat", (message) => {
                     const json = JSON.parse(message.body);
-                    setMessageList(prev=>[...prev, json]);//순서 보장
+                    setMessageList(prev => [...prev, json]); 
                 });
                 setConnect(true);
             },
-            onDisconnect: ()=>{
+            onDisconnect: () => {
                 setConnect(false);
             },
-            debug: (str)=>{
+            debug: (str) => {
                 console.log("[DEBUG] " + str);
             }
         });
+
+        if (login === true) {
+            client.connectHeaders = {
+                accessToken: accessToken,
+                refreshToken: refreshToken,
+            };
+        }
+
         client.activate();
         setClient(client);
-    }, [client, connect]);
-    const disconnectFromServer = useCallback(()=>{
-        if(client) {
+    }, [client, login, accessToken, refreshToken]);
+
+
+    const disconnectFromServer = useCallback(() => {
+        if (client) {
             client.deactivate();
         }
-    }, [client, connect]);
+    }, [client]);
 
-    const sendMessage = useCallback(()=>{
-        if(client === null) return;
-        if(connect === false) return;
-        if(input.length === 0) return;
+    const sendMessage = useCallback(() => {
+        if (client === null || connect === false || input.length === 0) return;
 
-        const json = {content : input};
+        const json = { content: input, sender: memberId };
 
-        client.publish({
+        const message = {
             destination: "/app/chat",
             body: JSON.stringify(json)
-        });
+        };
+
+        if (login === true) {
+            message.headers = {
+                accessToken: accessToken,
+                refreshToken: refreshToken
+            };
+        }
+
+        client.publish(message);
         setInput("");
-    }, [input, client, connect]);
+    }, [input, client, connect, login, accessToken, refreshToken, memberId]);
 
-
-    //view
-    return (<>
-        <Jumbotron title="웹소켓 클라이언트" 
-                content={"현재 연결 상태 = " + (connect ? "연결됨" : "종료됨")}/>
-        <div className="row mt-4">
-            <div className='col'>
-                <div className='input-group'>
-                    <input type="text" className="form-control"
-                            value={input} 
-                            onChange={e=>setInput(e.target.value)}/>
-                    <button className="btn btn-success"
+    // view
+    return (
+        <>
+            <Jumbotron title="웹소켓 클라이언트"
+                content={"현재 연결 상태 = " + (connect ? "연결됨" : "종료됨")} />
+            <div className="row mt-4">
+                <div className='col'>
+                    <div className='input-group'>
+                        <input type="text" className="form-control"
+                            value={input}
+                            onChange={e => setInput(e.target.value)}
+                            disabled={login === false}
+                        />
+                        <button className="btn btn-success"
+                            disabled={login === false}
                             onClick={sendMessage}>
-                        보내기
-                    </button>
-
+                            보내기
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div className="row mt-4">
-            <div className="col">
-                <ul className="list-group">
-                    {messageList.map((message, index)=>(
-                    <li className="list-group-item" key={index}>
-                        <p>{message.content}</p>
-                        <p className='text-muted'>{moment(message.time).format("a h:mm")}
-                            ({moment(message.time).fromNow()})
-                        </p>
-                    </li>
-                    ))}
-                </ul>
-
+            <div className="row mt-4">
+                <div className="col">
+                    <ul className="list-group">
+                        {messageList.map((message, index) => (
+                            <li className="list-group-item" key={index}>
+                                <div className={`row ${memberId === message.sender ? 'justify-content-end' : ''}`}>
+                                    <div className={`col-5 ${memberId === message.sender ? '' : 'offset-0'}`}>
+                                        <h3>
+                                            {message.sender}
+                                            <small>
+                                                ({message.level})
+                                            </small>
+                                        </h3>
+                                        <p>{message.content}</p>
+                                        <p className='text-muted'>{moment(message.time).format("a h:mm")}
+                                            ({moment(message.time).fromNow()})
+                                        </p>
+                                    </div>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
-        </div>
-    </>);
+        </>
+    );
 };
+
 export default WebSocket;
