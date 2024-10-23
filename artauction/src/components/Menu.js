6d4831d6fd0336c85691b051058b2554050cc870
Parkhewoo <pkhwoo91@gmail.com>
2024-10-23 14:16:20 +0900

    메뉴

--- artauction/src/components/Menu.js
+++ artauction/src/components/Menu.js
@@ -3,123 +3,90 @@ import { useCallback, useEffect, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { loginState, memberIdState, memberRankState } from "../utils/recoil";
+import Time from "./time/Time";
 
 
 const Menu = () => {
-  const navigate = useNavigate();
-  const [memberId, setMemberId] = useRecoilState(memberIdState);
-  const [memberRank, setMemberRank] = useRecoilState(memberRankState);
-  const login = useRecoilValue(loginState);
+    const navigate = useNavigate();
+    const [memberId, setMemberId] = useRecoilState(memberIdState);
+    const [memberRank, setMemberRank] = useRecoilState(memberRankState);
+   
+    const login = useRecoilValue(loginState);
 
-  const [time, setTime] = useState();
-  const [timeToShow, setTimeToShow] = useState();
-
-  const getTime = useCallback(async () => {
-    try {
-      const resp = await axios.get("http://localhost:8080/time/");
-      const currentTime = resp.data;
-      const date = new Date(currentTime);
-      const hours = date.getHours().toString().padStart(2, '0');
-      const minutes = date.getMinutes().toString().padStart(2, '0');
-      const seconds = date.getSeconds().toString().padStart(2, '0');
-      setTime(date);
-      setTimeToShow(`${hours}:${minutes}:${seconds}`);
-    } catch (error) {
-      console.error("시간 요청 오류:", error);
-    }
-  }, []);
-
-  const afterSecond = useCallback(() => {
-    setTime((prevTime) => {
-      if (!prevTime) return;
+    const logout = useCallback(() => {
+        setMemberId("");
+        setMemberRank("");
+        delete axios.defaults.headers.common["Authorization"];
+        window.localStorage.removeItem("refreshToken");
+        window.sessionStorage.removeItem("refreshToken");
+        navigate("/");
+    }, [navigate, setMemberId, setMemberRank]);
 
 
-      const date = new Date(prevTime);
-      date.setSeconds(date.getSeconds() + 1);
-      const hours = date.getHours().toString().padStart(2, '0');
-      const minutes = date.getMinutes().toString().padStart(2, '0');
-      const seconds = date.getSeconds().toString().padStart(2, '0');
+    return (
+        <>
+            <nav className="navbar navbar-expand bg-light fixed-top" data-bs-theme="light">
+                <div className="container-fluid">
+                    <div className="collapse navbar-collapse" id="top-menu">
+                        <ul className="navbar-nav me-auto">
+                            { }
 
-      setTimeToShow(`${hours}:${minutes}:${seconds}`);
-      return date;
-    });
-  }, []);
+                            {login ? (<>
+                                <li className="nav-item">
+                                    <NavLink className="nav-link" to="/member/mypage">
+                                        {memberId}
+                                        ({memberRank})
+                                    </NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink className="nav-link" to="/"
+                                        onClick={logout}>
+                                        로그아웃
+                                    </NavLink>
+                                </li>
+                            </>) : (<>
+                                <li className="nav-item">
+                                    <NavLink className="nav-link" to="/join">
+                                        회원가입
+                                    </NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink className="nav-link" to="/login">
+                                        로그인
+                                    </NavLink>
+                                </li>
+                            </>)}
 
-  useEffect(() => {
-    getTime();
-    const intervalId = setInterval(() => {
-      getTime();
-    }, 30000);
-    return () => clearInterval(intervalId);
-  }, [getTime]);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      afterSecond();
-    }, 1000);
-    return () => clearInterval(intervalId);
-  }, [afterSecond]);
-
-  const logout = useCallback(() => {
-    setMemberId("");
-    setMemberRank("");
-    delete axios.defaults.headers.common["Authorization"];
-    window.localStorage.removeItem("refreshToken");
-    window.sessionStorage.removeItem("refreshToken");
-    navigate("/");
-  }, [navigate, setMemberId, setMemberRank]);
-
-  return (
-    <>
-      <nav className="navbar navbar-expand bg-light fixed-top" data-bs-theme="light">
-        <div className="container-fluid">
-          <div className="collapse navbar-collapse" id="top-menu">
-            <ul className="navbar-nav me-auto">
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/join">회원가입</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/login">로그인</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/artist">작가</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/auctionschedule">경매</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/work/list">작품</NavLink>
-              </li>
-            </ul>
-            <ul className="navbar-nav me-auto">
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/notice">공지사항</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/faq">FAQ</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/websocket">websocket</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/charge">포인트 충전하기</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/payment">결제하기</NavLink>
-              </li>
-            </ul>
-            <ul className="navbar-nav me-auto">
-              <li className="nav-item">
-                <NavLink className="nav-link" to="#">{timeToShow && (<div>{timeToShow}</div>)}</NavLink>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </>
-  );
-
-
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/artist">작가</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/auctionschedule">경매</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/work/list">작품</NavLink>
+                            </li>
+                        </ul>
+                        <ul className="navbar-nav me-auto">
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/notice">공지사항</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/faq">FAQ</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/websocket">websocket</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/charge">포인트 충전하기</NavLink>
+                            </li>
+                        </ul>
+                        
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
 };
 
 export default Menu;
